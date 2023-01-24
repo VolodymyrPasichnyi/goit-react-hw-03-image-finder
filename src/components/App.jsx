eebb311012dd19ec47d2b7248f4e0883547fd6d3
Volodymyr Pasichnyi <vlpasnj33@gmail.com>
2023-01-24 17:15:00 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
+import { Component } from "react"
+
+
+
+
+export class App extends Component {
+  state = {
+    gallery: null,
+  }
+  componentDidMount () {
+    fetch(`https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12`)
+      .then(res => res.json())
+      .then(img => this.setState({ img }));
+  }
+
+  render() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      {this.state.gallery && (
+        <div>FKLALSdkdads</div>
+      )}
     </div>
   );
-};
+}
+}
+
